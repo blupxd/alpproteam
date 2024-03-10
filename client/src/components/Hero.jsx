36b035d4027076e1be2af58897e2bcdd63fe6135
blupxd <matija.stefanovic050@gmail.com>
2024-03-10 17:19:54 +0100

    xd

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
   return (
     <div id="pocetna" className='bg-white mt-24 max-h-[1000px] md:mt-20 mb-24'>
         <div className='grid grid-cols-1 md:grid-cols-2 my-4'>
-            <div className='p-4 md:p-12 flex md:text-left text-center md:items-start items-center justify-center flex-col gap-4'>
+            <div className='p-8 md:p-12 flex md:text-left text-center md:items-start items-center justify-center flex-col gap-4'>
                 <h1 className='text-gray-600 font-semibold text-7xl md:text-8xl'>
                     <span className='text-blue-600 font-bold'>ALP-PRO</span> Team
                 </h1>
-                <h3 className='w-96 md:border-b-0 md:border-l-[4px] pb-4 pl-0 md:pl-4 border-amber-500 text-gray-600 font-light text-3xl md:text-2xl'>
+                <h3 className='w-96 md:border-b-0 md:border-l-[4px] p-4 md:pl-4 border-amber-500 text-gray-600 font-light text-3xl md:text-2xl'>
                     Visoko iznad očekivanja! Profesionalni alpinistički rad, jer svaki vrhunski detalj čini razliku.
                 </h3>
                 <div className='flex flex-row mt-14 gap-4 mb-14 items-center'>
