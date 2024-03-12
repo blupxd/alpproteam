dbad33b997d8d6092cbffc89e5a86667ccf93e6c
blupxd <67112013+blupxd@users.noreply.github.com>
2024-03-12 22:26:02 +0100

    Update galerijaRoute.js

--- server/routes/galerijaRoute.js
+++ server/routes/galerijaRoute.js
@@ -12,8 +12,8 @@ const storage = getStorage();
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+router.use(express.json({ limit: '5mb' }));
+router.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 function verifyToken(req, res, next) {
     const authHeader = req.headers['authorization'];
