b9ac85603446ec745edbdae1acab4d3f8c692a99
blupxd <67112013+blupxd@users.noreply.github.com>
2024-03-12 22:34:57 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(upload.any());
 app.use('/posao', posaoRoute);
 app.use('/auth', authRoute);
 app.use('/galerija', galerijaRoute);
-app.use(upload.any());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
