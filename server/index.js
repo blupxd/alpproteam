f50c760232b034b79f0fa21411ab15b1bf83aa41
blupxd <matija.stefanovic050@gmail.com>
2024-03-12 22:54:48 +0100

    xd

--- server/index.js
+++ server/index.js
@@ -13,11 +13,13 @@ const app = express();
 
 app.use(cors());
 app.use(upload.any());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use('/posao', posaoRoute);
 app.use('/auth', authRoute);
 app.use('/galerija', galerijaRoute);
-app.use(express.json());
-app.use(express.urlencoded());
+
 
 mongoose
   .connect(process.env.MONGO_URI)
